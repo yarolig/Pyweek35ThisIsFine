269390bc30b89b84fbedafa8458eb82adbff42e3
Alexander Izmailov <yarolig@gmail.com>
2023-03-29 19:49:43 +0300

    basic snake movement

--- gamelib/__init__.py
+++ gamelib/__init__.py
@@ -5,41 +5,15 @@ import pyglet.font
 from pyglet.graphics.shader import Shader, ShaderProgram
 
 
-default_vs = """#version 150 core
-    in vec3 position;
-    in vec4 colors;
-    out vec4 vertex_colors;
-
-    uniform mat4 projection;
-
-    void main()
-    {
-        gl_Position = projection * vec4(position, 1.0);
-        vertex_colors = colors;
-    }
-"""
-
-default_fs = """#version 150 core
-    in vec4 vertex_colors;
-    out vec4 final_color;
-
-    void main()
-    {
-        final_color = vertex_colors*0 + vec4(1,0,0,0);
-    }
-"""
 class GameWindow(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fps_display = pyglet.window.FPSDisplay(self)
         self.set_caption("Pyweek35 This is fine")
-        self.default_shader = ShaderProgram(Shader(default_vs, 'vertex'),
-                                            Shader(default_fs, 'fragment'))
-
-        print(self.default_shader.attributes)
-        self.vbo1 = self.default_shader.vertex_list(3, gl.GL_TRIANGLES)
-        print(self.vbo1.position[:])
-        self.vbo1.position[:] = (0, 0, 0, 100, 100, 100)
+        self.keys = pyglet.window.key.KeyStateHandler()
+        self.push_handlers(self.keys)
+        self.started = False
+        self.ticks = 0
 
         #pyglet.font.add_directory("data/fonts")
         #self.font = pyglet.font.load("Funtype", 8, False, False)
@@ -50,16 +24,175 @@ class GameWindow(pyglet.window.Window):
         #                                font_name="Constantine",
         #                                font_size=12,x=10, y=10)
 
+    def on_maybe_start(self):
+        if self.started:
+            return
+        self.started = True
+
+        self.main = Main()
+        self.level = Level()
+
+        self.level.cell(3,3).front_entity = SnakeHead()
+        self.level.cell(3,4).front_entity = SnakeBody()
+        self.level.cell(4,4).front_entity = SnakeTail()
+
+        self.level.cell(6,7).front_entity = Apple()
+
+
     def on_draw(self, dt=0):
+        self.on_maybe_start()
+        self.ticks += 1
+
+
+
+        if self.ticks % 20 == 0:
+            self.main.macro_tick(self.level, self.keys)
+
+
         gl.glClearColor(0.2, 0.3, 0.4, 1.0)
         self.clear()
-
+        self.main.draw_level(self.level)
         self.fps_display.draw()
+
+
 
     def on_resize(self, width, height):
         gl.glViewport(0, 0, width, height)
 
+all_images = {}
+def get_image(name):
+    global  all_images
+    if name in all_images:
+        return all_images[name]
+    all_images[name] = pyglet.image.load(name)
+    return all_images[name]
 
+DIRECTIONS = 'WDSA'
+DIRECTIONS_XY = {
+    "W" : (0, 1),
+    "S" : (0, -1),
+    "A" : (-1, 0),
+    "D" : (1, 0),
+}
+
+
+class Entity:
+    x = 0
+    y = 0
+    image_template = ''
+
+    # points to moving direction or connected head-side body part
+    direction = ''
+
+    # points to tail-side connected body part
+    connection = ''
+    def get_image_name(self):
+        return self.image_template.replace('X', self.direction)
+
+class SnakeHead(Entity):
+    direction = 'W'
+    image_template = 'data/pics/snake_headX.png'
+    tail_x = 0
+    tail_y = 0
+
+class SnakeBody(Entity):
+    direction = 'W'
+    image_template = 'data/pics/snake_bodyX.png'
+
+class SnakeTail(Entity):
+    direction = 'W'
+    image_template = 'data/pics/snake_tailX.png'
+
+class Apple(Entity):
+    direction = 'W'
+    image_template = 'data/pics/apple.png'
+
+class Lamp(Entity):
+    direction = 'W'
+    image_template = 'data/pics/lamp.png'
+
+
+class Cell:
+    x = 0
+    y = 0
+    back_entity = None
+    front_entity = None
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Level:
+    def __init__(self):
+        self.w = 8
+        self.h = 8
+        self.cells = [Cell(a,b)
+            for b in range(self.h)
+            for a in range(self.w)]
+        self.player_head = None
+
+    def cell(self, x ,y) -> Cell:
+        if (0 <= x < self.w) and (0 <= y < self.h):
+            return self.cells[x+y*self.w]
+        return  None
+
+    def enum_cells(self):
+        for b in range(self.h):
+            for a in range(self.w):
+                yield a, b, self.cell(a,b)
+
+class Main:
+    def __init__(self):
+        self.foreground_batch = pyglet.graphics.Batch()
+        self.foreground_batch_sprites = []
+
+    def draw_level(self, level):
+        self.foreground_batch_sprites = []
+        for a, b, c in level.enum_cells():
+            assert isinstance(c, Cell)
+            if c.front_entity:
+                assert isinstance(c.front_entity, Entity)
+                img_name = c.front_entity.get_image_name()
+                s = pyglet.sprite.Sprite(get_image(img_name), a*64, b*64)
+                s.draw()
+        self.foreground_batch.draw()
+
+    def macro_tick(self, level, keys):
+        # find snake's head
+        # try to move head in its direction
+        # pull conected body parts :)
+        # update body parts location
+        print('macro tick')
+        head_cell = None
+        for a, b, c in level.enum_cells():
+#            print (b,a,c.front_entity)
+            if c.front_entity and isinstance(c.front_entity, SnakeHead):
+                head_cell = c
+                level.player_head = c
+                print('head is at', a, b)
+                break
+
+        if head_cell:
+            dd = head_cell.front_entity.direction
+            if keys[pyglet.window.key.W]:
+                dd = 'W'
+            if keys[pyglet.window.key.S]:
+                dd = 'S'
+            if keys[pyglet.window.key.A]:
+                dd = 'A'
+            if keys[pyglet.window.key.D]:
+                dd = 'D'
+
+            dx, dy = DIRECTIONS_XY[dd]
+            newx, newy = head_cell.x + dx, head_cell.y + dy
+            tgt_cell = level.cell(newx, newy)
+            if tgt_cell and not tgt_cell.front_entity:
+                tgt_cell.front_entity = c.front_entity
+                c.front_entity = None
+            #else:
+            #    print("can't move to", newx, newy, tgt_cell, tgt_cell.front_entity)
+
+        else:
+            print('no head')
 
 def main():
     print('qwe')
